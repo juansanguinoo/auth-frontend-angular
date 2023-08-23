2c1a088dc20a6791b3e85b90cd2766dfbea699f1
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-08-23 15:05:06 -0500

    login implementation

--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DashboardLayoutComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  public user = computed(() => this.authService.currentUser());
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
